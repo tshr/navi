4b61341ad57bbc166cee5c0ea1c54987cc8ee02c
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-08-23 08:26:35 -0700

    added semicolons

--- navi.js
+++ navi.js
@@ -78,12 +78,12 @@
     }
   };
 
-  Navi.get_listeners = function(object) { return register.get(object) };
+  Navi.get_listeners = function(object) { return register.get(object); };
 
-  Navi.unregister = function(object) { register.remove(object) };
+  Navi.unregister = function(object) { register.remove(object); };
 
-  Navi.inspect_register = function() { return register.entries() };
+  Navi.inspect_register = function() { return register.entries(); };
 
-  Navi.clear_register = function() { register.clear() };
+  Navi.clear_register = function() { register.clear(); };
 
 }());
