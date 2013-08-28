eb720a499e52e69ee3ad5f3723e0e30e779739d6
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-08-27 21:59:01 -0700

    specs wip

--- spec/javascripts/navi_spec.js
+++ spec/javascripts/navi_spec.js
@@ -1,18 +1,58 @@
-describe("Navi", function() {
-  it("should be a global object", function() {
-    expect(typeof(Navi)).toEqual("object");
+describe('Navi', function() {
+
+  var notifier, listener_1, listener_2;
+
+  beforeEach(function() {
+
+    notifier = {"name" : "notifier"};
+    listener_1 = { 'update_1' : function(){} };
+    listener_2 = { 'update_2' : function(){} };
+
+    Navi.listen( listener_1, notifier, 'update_1' );
+    Navi.listen( listener_2, notifier, 'update_2' );
+
+    spyOn( listener_1, 'update_1' );
+    spyOn( listener_2, 'update_2' );
+
   });
 
-  it("should have notify, listen, unlisten, get_listeners, unregister, inspect_register, clear_register methods", function() {
-    expect( typeof (Navi.notify) ).toEqual("function");
-    expect( typeof (Navi.listen) ).toEqual("function");
-    expect( typeof (Navi.unlisten) ).toEqual("function");
-    expect( typeof (Navi.get_listeners) ).toEqual("function");
-    expect( typeof (Navi.unregister) ).toEqual("function");
-    expect( typeof (Navi.inspect_register) ).toEqual("function");
-    expect( typeof (Navi.clear_register) ).toEqual("function");
+  afterEach(function() {
+    Navi.clear_register();
   });
 
+  it('should call update_1 and update_2 on listener_1 and listener_2 when notify is called with notifier', function() {
+    Navi.notify(notifier);
+    expect(listener_1.update_1).toHaveBeenCalled();
+    expect(listener_2.update_2).toHaveBeenCalled();
+  });
 
+  it('should return an array containing arrays of listener_1 and listener_2 and their corresponding registered function names when get_listeners is called with notifier', function() {
+    var listeners = Navi.get_listeners(notifier);
+    expect(listeners.length).toEqual(2);
+
+    expect(listeners[0][0]).toEqual(listener_1);
+    expect(listeners[0][1]).toEqual('update_1');
+
+    expect(listeners[1][0]).toEqual(listener_2);
+    expect(listeners[1][1]).toEqual('update_2');
+  });
+
+  it("should remove a listener from an object's listeners when unlisten is called with the listener and the object", function() {
+    var listeners = Navi.get_listeners(notifier);
+    expect(listeners.length).toEqual(2);
+
+    expect(listeners[0][0]).toEqual(listener_1);
+    expect(listeners[0][1]).toEqual('update_1');
+
+    Navi.unlisten(listener_1, notifier);
+
+    expect(listeners.length).toEqual(1);
+    expect(listeners[0][0]).toEqual(listener_2);
+  });
+
+  /*it("should return the register's entries when inspect_register is called", function() {
+    window.entries = Navi.inspect_register();
+    console.log(entries);
+  });*/
 
 });
