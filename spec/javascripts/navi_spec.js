39eb90104d5deb65e7a63c7bfa4987a21e53c7c1
tshr <tshr@Toshiros-MacBook-Pro.local>
2013-09-02 15:18:22 -0700

    stopped using jshashtable, storing listeners on object, added Navi function to augment objects with Navi functions

--- spec/javascripts/navi_spec.js
+++ spec/javascripts/navi_spec.js
@@ -4,48 +4,49 @@ describe('Navi', function() {
 
   beforeEach(function() {
 
-    notifier = {'name' : 'notifier'};
-    listener_1 = { 'update_1' : function(object){} };
-    listener_2 = { 'update_2' : function(object){} };
+    notifier   = Navi({'name' : 'notifier'});
+    listener_1 = Navi({ 'update_1' : function(object){} });
+    listener_2 = Navi({ 'update_2' : function(object){} });
 
-    Navi.listen( notifier, listener_1, 'update_1' );
-    Navi.listen( notifier, listener_2, 'update_2' );
+    listener_1.listen(notifier, 'update_1');
+    listener_2.listen(notifier, 'update_2');
 
   });
 
   afterEach(function() {
-    Navi.clear_register();
+    delete notifier.listeners;
   });
 
-  it('should call update_1 and update_2 on listener_1 and listener_2 when notify is called with notifier', function() {
+  it('should call the registered methods on its listeners when notify is called on the publishing object', function() {
 
     spyOn( listener_1, 'update_1' );
     spyOn( listener_2, 'update_2' );
 
-    Navi.notify(notifier);
+    notifier.notify()
 
     expect(listener_1.update_1).toHaveBeenCalledWith(notifier);
     expect(listener_2.update_2).toHaveBeenCalledWith(notifier);
 
   });
 
-  it("should update the listener and its registered method if listen is called with a pre-registered listener / object pair", function() {
+  it("should update the listener's registered method if listen is called on a pre-registered listener", function() {
 
     listener_1.new_method = function(object) {};
 
     spyOn( listener_1, 'update_1' );
     spyOn( listener_1, 'new_method' );
 
-    Navi.listen(notifier, listener_1, "new_method");
-    Navi.notify(notifier);
+    listener_1.listen(notifier, "new_method");
+
+    notifier.notify();
 
     expect(listener_1.new_method).toHaveBeenCalledWith(notifier);
     expect(listener_1.update_1).not.toHaveBeenCalledWith(notifier);
   });
 
-  it('should return an array of arrays containing listener_1 and listener_2 and their registered function names when get_listeners is called with notifier', function() {
+  it("should return an array of object's listeners and their registered method names when get_listeners is called on the notifier", function() {
 
-    var listeners = Navi.get_listeners(notifier);
+    var listeners = notifier.get_listeners();
     expect(listeners.length).toEqual(2);
 
     expect(listeners[0].listener).toEqual(listener_1);
@@ -56,39 +57,33 @@ describe('Navi', function() {
 
   });
 
-  it("should remove a listener from an object's listeners when unlisten is called with the listener and the object", function() {
+  it("should return an empty array of get_listeners is called on an object that has no listeners", function() {
+    var has_no_listeners = Navi({});
+    expect(has_no_listeners.get_listeners()).toEqual([]);
+  });
 
-    var listeners = Navi.get_listeners(notifier);
+  it("should remove a listener from an object's listeners when unlisten is called on the listener with the object", function() {
 
-    Navi.unlisten(listener_1, notifier);
+    var listeners = notifier.get_listeners();
+
+    listener_1.unlisten(notifier);
 
     expect(listeners.length).toEqual(1);
     expect(listeners[0].listener).toEqual(listener_2);
   });
 
-  it('should remove notifier from the registry when unregister is called with it', function() {
+  it("should delete the object's listeners attribute if unlisten is called on the last remaining listener", function() {
 
-    Navi.unregister(notifier);
-    expect(Navi.inspect_register().length).toEqual(0);
+    listener_1.unlisten(notifier);
+    listener_2.unlisten(notifier);
 
+    expect(typeof notifier.listeners).toEqual("undefined");
   });
 
-  it("should return the register's entries when inspect_register is called", function() {
+  it("should delete an object's listeners attribute when clear_listeners is called", function() {
 
-    var entries = Navi.inspect_register();
-
-    expect(entries.length).toEqual(1);
-    expect(entries[0][0]).toEqual(notifier);
-    expect(entries[0][1][0].listener).toEqual(listener_1);
-    expect(entries[0][1][0].method_name).toEqual("update_1");
-    expect(entries[0][1][1].listener).toEqual(listener_2);
-    expect(entries[0][1][1].method_name).toEqual("update_2");
-  });
-
-  it('should clear the register when clear_register is called', function() {
-
-    Navi.clear_register();
-    expect(Navi.inspect_register().length).toEqual(0);
+    notifier.clear_listeners();
+    expect(typeof notifier.listeners).toEqual("undefined");
 
   });
 });
