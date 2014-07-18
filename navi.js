20dd32b22a23e3fd88219282466e9462efbf2f40
tshr <toshirokensugihara@Toshiros-MacBook-Pro.local>
2014-07-18 01:05:39 -0700

    update gitignore, reference functions in closure, rerun minify

--- navi.js
+++ navi.js
@@ -7,51 +7,57 @@
 
 (function(){
 
+  var addListener = function(listener, methodName) {
+    this.getListeners().push({ 'listener' : listener, 'methodName' : methodName });
+  };
+
+  var removeListener = function(listener) {
+    var listeners = this.getListeners();
+    for (var i = 0; i < listeners.length; i++) {
+      if (listeners[i].listener === listener) listeners.splice(i, 1);
+      break;
+    }
+  };
+
+  var listen = function(notifier, methodName) {
+
+    notifier.removeListener(this);
+    notifier.addListener(this, methodName);
+  };
+
+  var unlisten = function(notifier) {
+    notifier.removeListener(this);
+  };
+
+  var notify = function() {
+
+    var listeners = this.getListeners();
+    // call stored method names on stored listeners and pass in the notifying object
+    for (var i = 0; i < listeners.length; i++) {
+      var listenerObject = listeners[i];
+      listenerObject.listener[listenerObject.methodName](this);
+    }
+  };
+
   this.Navi = function(object) {
 
     var listeners = [];
 
-    var getListeners = function() { // defined locally first because it's called privately by notify
+    var getListeners = function() {
       return listeners;
     };
 
-    object.getListeners = getListeners;
-
-    object.addListener = function(listener, method_name) {
-      listeners.push({ 'listener' : listener, 'method_name' : method_name });
-    };
-
-    object.removeListener = function(listener) {
-
-      for (var i = 0; i < listeners.length; i++) {
-        if (listeners[i].listener === listener) listeners.splice(i, 1);
-        break;
-      }
-    };
-
-    object.listen = function(notifier, method_name) {
-
-      notifier.removeListener(this);
-      notifier.addListener(this, method_name);
-    };
-
-    object.unlisten = function(notifier) {
-      notifier.removeListener(this);
-    };
-
-    object.notify = function() {
-
-      var listeners = getListeners();
-      // call stored method names on stored listeners and pass in the notifying object
-      for (var i = 0; i < listeners.length; i++) {
-        var listenerObject = listeners[i];
-        listenerObject.listener[listenerObject.method_name](this);
-      }
-    };
-
-    object.clearListeners = function() {
+    var clearListeners = function() {
       listeners = [];
     };
+
+    object.getListeners = getListeners;
+    object.addListener = addListener;
+    object.removeListener = removeListener;
+    object.listen = listen;
+    object.unlisten = unlisten;
+    object.notify = notify;
+    object.clearListeners = clearListeners;
 
     return object;
   };
